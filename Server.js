dbee19155f3c6db5b3b56fa958e97131fc7c4851
Rawal Ritesh <37304640+Ritzrawal@users.noreply.github.com>
2018-09-19 10:37:07 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -6,14 +6,11 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
-
 mongoose.connect('mongodb://localhost/ManualAuth', { useMongoClient: true });
-
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
 });
-
 app.use(session({
   secret: 'work hard',
   resave: true,
@@ -22,24 +19,18 @@ app.use(session({
     mongooseConnection: db
   })
 }));
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');	
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(express.static(__dirname + '/views'));
-
 var index = require('./routes/index');
 app.use('/', index);
-
 app.use(function (req, res, next) {
   var err = new Error('File Not Found');
   err.status = 404;
   next(err);
 });
-
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.send(err.message);
@@ -47,5 +38,5 @@ app.use(function (err, req, res, next) {
 
 
 app.listen(3000, function () {
-  console.log('Express app listening on port 3000');
+  console.log('Running 3000');
 });
